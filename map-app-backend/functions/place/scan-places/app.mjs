67d6d1b87d6e4eb837ae238cd76d51e48a349801
Maxwell Henderson <maxwellc.henderson@gmail.com>
2024-07-19 14:33:39 -0600

    list / categories updates

--- map-app-backend/functions/place/scan-places/app.mjs
+++ map-app-backend/functions/place/scan-places/app.mjs
@@ -15,5 +15,32 @@ export const lambdaHandler = async (event) => {
   const listId = event.queryStringParameters.listId;
   console.log("listId: ", listId);
 
-//   get places with 
+  //   get places with gsi listId
+  const params = {
+    TableName: PLACES_TABLE,
+    IndexName: "listId-index",
+    KeyConditionExpression: "listId = :listId",
+    ExpressionAttributeValues: {
+      ":listId": { S: listId },
+    },
+  };
+
+  try {
+    const data = await dbclient.send(new QueryCommand(params));
+    console.log("Data: ", data.Items[0]);
+    const places = data.Items;
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(places),
+    };
+  } catch (err) {
+    console.log("Error: ", err);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify(err),
+    };
+  }
 };
