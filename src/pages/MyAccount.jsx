c5825a19be184630b04dc7aa54f32b437fc001ac
Maxwell Henderson <maxwellc.henderson@gmail.com>
2024-08-16 06:21:44 -0400

    appUser added to my account page

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import SignOutButton from "../components/Authentication/SignoutButton";
 
+import useAppUser from "../hooks/backend-hooks/useAppUser";
+
 const MyAccount = () => {
+  const { appUser } = useAppUser();
+
+  useEffect(() => {
+    console.log(appUser);
+  }, [appUser]);
+
   return (
     <div>
       <h1>My Account</h1>
